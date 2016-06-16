3ca24270b29c16e24f3e739b44c964b8396be80a
EMG114 <egutierrez411@gmail.com>
2016-06-16 15:37:46 -0400

    done

--- OOPCardsSimpleViews/FISCardSwitcherViewController.m
+++ OOPCardsSimpleViews/FISCardSwitcherViewController.m
@@ -3,6 +3,12 @@
 #import "FISCardSwitcherViewController.h"
 
 @interface FISCardSwitcherViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *topLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *middleLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *bottomLabel;
+
 
 @end
 
@@ -14,12 +20,71 @@
     // Do any additional setup after loading the view.
     self.threeOfSpades = [[FISCard alloc] initWithSuit:@"♠️"
                                                   rank:@"3"];
+    
+    
     self.fourOfClubs = [[FISCard alloc] initWithSuit:@"♣️"
                                                 rank:@"4"];
+    
+    
     self.eightOfDiamonds = [[FISCard alloc] initWithSuit:@"♦️"
                                                     rank:@"8"];
+    
+    
     self.tenOfHearts = [[FISCard alloc] initWithSuit:@"♥️"
                                                 rank:@"10"];
 }
+
+- (IBAction)threeOfSpades:(id)threeOfSpades {
+    
+    self.topLabel.text =self.threeOfSpades.cardLabel;
+    
+    self.middleLabel.text =self.threeOfSpades.cardLabel;
+    
+    self.bottomLabel.text =self.threeOfSpades.cardLabel;
+    
+}
+
+- (IBAction)fourOfClubs:(id)sender {
+    
+    self.topLabel.text =self.fourOfClubs.cardLabel;
+    
+    self.middleLabel.text =self.fourOfClubs.cardLabel;
+    
+    self.bottomLabel.text =self.fourOfClubs.cardLabel;
+    
+}
+
+- (IBAction)eightOfDiamonds:(id)sender {
+    
+    
+    self.topLabel.text =self.eightOfDiamonds.cardLabel;
+    
+    self.middleLabel.text =self.eightOfDiamonds.cardLabel;
+    
+    self.bottomLabel.text =self.eightOfDiamonds.cardLabel;
+    
+
+
+    
+}
+
+
+- (IBAction)tenOfHearts:(id)sender {
+    
+    self.topLabel.text =self.tenOfHearts.cardLabel;
+    
+    self.middleLabel.text =self.tenOfHearts.cardLabel;
+    
+    self.bottomLabel.text =self.tenOfHearts.cardLabel;
+    
+    
+}
+
+
+
+
+
+
+
 
 @end
